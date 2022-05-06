876513fbf97ce34c2d61c354540a284938cda44a
Falko Hilbert <fhilbert@viosys.com>
2022-05-06 11:10:53 +0200

    remove composer compile and use UpdatePostFinishEvent instead to trigger migration execution

---
+++ src/Subscriber/SystemSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace Viosys\VioDBMigration\Subscriber;
+
+use Shopware\Core\Framework\Update\Event\UpdatePostFinishEvent;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class SystemSubscriber implements EventSubscriberInterface
+{
+    protected KernelInterface $kernel;
+    protected string $migrationNamespace;
+
+    public function __construct(
+        KernelInterface $kernel,
+        string $migrationNamespace = 'VioDbMigration'
+    )
+    {
+        $this->kernel = $kernel;
+        $this->migrationNamespace = $migrationNamespace;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            UpdatePostFinishEvent::class => 'onPostFinish',
+        ];
+    }
+
+    public function onPostFinish(UpdatePostFinishEvent $event)
+    {
+        $application = new Application($this->kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            0 => 'database:migrate',
+            '--no-interaction' => null,
+            '--all' => null,
+            'identifier' => $this->migrationNamespace,
+        ]);
+        $application->run($input);
+    }
+
+}
